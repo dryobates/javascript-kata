1a6a21c25b7374a78bd225c5869010df9f416c53
dryobates <jakub.stolarski@gmail.com>
2019-06-07 08:27:08 +0200

    OO version.

--- leap_years/leap.js
+++ leap_years/leap.js
@@ -1,19 +1,25 @@
 const FIRST_GREGORIAN_YEAR = 1583
 
-function isLeap (year) {
-  if (year < FIRST_GREGORIAN_YEAR) {
-    throw new NotGregorianYear('Before october 1582 there was no gregorian calendar. Given year: ' + year)
+class Year {
+  constructor (year) {
+    this.year = year
   }
-  if ((year % 400) === 0) {
-    return true
+  isLeap () {
+    if (this.year < FIRST_GREGORIAN_YEAR) {
+      throw new NotGregorianYear('Before october 1582 there was no gregorian calendar. Given year: ' + this.year)
+    }
+    if ((this.year % 400) === 0) {
+      return true
+    }
+    if ((this.year % 100) === 0) {
+      return false
+    }
+    return ((this.year % 4) === 0)
   }
-  if ((year % 100) === 0) {
-    return false
-  }
-  return ((year % 4) === 0)
 }
 
 function NotGregorianYear (message) {
   this.message = message
 }
-module.exports = [isLeap, NotGregorianYear]
+
+module.exports = [Year, NotGregorianYear]
--- leap_years/leap.test.js
+++ leap_years/leap.test.js
@@ -1,29 +1,33 @@
-const [isLeap, NotGregorianYear] = require('./leap.js')
+const [Year, NotGregorianYear] = require('./leap.js')
 
 test('is not leap when not divisible by 4', () => {
-  expect(isLeap(2001)).toBe(false)
+  expect(new Year(2001).isLeap()).toBe(false)
 })
 
 test('is leap when divisible by 4', () => {
-  expect(isLeap(2004)).toBe(true)
+  expect(new Year(2004).isLeap()).toBe(true)
 })
 
 test('is not leap when divisible by 100', () => {
-  expect(isLeap(1900)).toBe(false)
+  expect(new Year(1900).isLeap()).toBe(false)
 })
 
 test('is leap when divisible by 400', () => {
-  expect(isLeap(2000)).toBe(true)
+  expect(new Year(2000).isLeap()).toBe(true)
 })
 
 test('raises not gregorian year when before 1583', () => {
-  expect(() => { isLeap(1500) }).toThrow(NotGregorianYear)
+  expect(() => { new Year(1500).isLeap() }).toThrow(NotGregorianYear)
 })
 
 test('converts to int when float', () => {
-  expect(isLeap(2004.0)).toBe(true)
+  expect(new Year(2004.0).isLeap()).toBe(true)
 })
 
 test('converts to int when string', () => {
-  expect(isLeap('2004')).toBe(true)
+  expect(new Year('2004').isLeap()).toBe(true)
+})
+
+test('is not leap when not divisible by 4', () => {
+  expect(new Year(2001).isLeap()).toBe(false)
 })
